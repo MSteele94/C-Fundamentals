0d39b18d3776c4d7a0dfcc1718c56a79173f3a95
MSteele94 <bengalsfan620@gmail.com>
2021-03-09 14:08:32 -0500

    Add project files.

---
+++ ReferenceTypes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReferenceTypes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReferenceTypes")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9c21bbe9-1e90-4636-83ed-2b1f31522301")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReferenceTypes/UnitTest1.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ReferenceTypes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Strings()
+        {
+            string name;
+
+            string declared;
+            declared = "this is initialized.";
+
+            string declareAndInitialize = "This is both declaring and initializing.";
+
+            string firstName = "Michael";
+            string lastName = "Steele";
+
+            string concatenatedFullName = firstName +  " " + lastName;
+
+            string interpolationFullName = $"{firstName} {lastName}";
+
+            //composite always has the "string.Format"
+            string compositeFullName = string.Format("{0} {1}", firstName, lastName);
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(compositeFullName);
+            Console.WriteLine(interpolationFullName);
+            Console.WriteLine(compositeFullName);
+            
+        }
+        [TestMethod]
+        public void Collections()
+        {
+            string stringExample = "Hello World";
+
+            string[] stringArray = { "Hello", "World!", "Why", "is it", "always", stringExample };
+            string[] somethingDifferent = { "some", "word" };
+            
+            //Arrays start with 0, not 1
+
+            string thirdItem = stringArray[2];
+            Console.WriteLine(thirdItem);
+
+            somethingDifferent[1] = "idk";
+            Console.WriteLine(somethingDifferent[1]);
+
+            stringArray[0] = "Hey There";
+            Console.WriteLine(stringArray[0]);
+
+            //always select the "using" option if available when using the lightbulb quickfix
+
+            List<string> listOfStrings = new List<string>();
+            List<int> listOfInts = new List<int>();
+
+            listOfStrings.Add("First string for our list");
+
+            listOfInts.Add(123456);
+
+            Console.WriteLine(listOfInts[0]);
+            //queue will display first item
+            //queue is good for help line application like a call center
+            Queue<string> firstInFirstOut = new Queue<string>();
+
+            firstInFirstOut.Enqueue("I'm First");
+            firstInFirstOut.Enqueue("I'm Next");
+
+            string firstItem = firstInFirstOut.Dequeue();
+            Console.WriteLine(firstItem);
+            
+            // Dictionaries have infinite amounts
+            Dictionary<int, string> keyAndValue = new Dictionary<int, string>();
+
+            keyAndValue.Add(7, "Agent");
+
+            string valueSeven = keyAndValue[7];
+            Console.WriteLine(valueSeven);
+            
+            
+            //these are other collection types
+
+            //sorted list will always put the key where it goes
+            //rarely used
+            SortedList<int, string> sortedKeyandValue = new SortedList<int, string>();
+            HashSet<int> uniqueList = new HashSet<int>();
+            Stack<string> lastInFirstOut = new Stack<string>();
+        }
+        [TestMethod]
+        public void Classes()
+        {
+            Random rng = new Random();
+
+            int randomNumber = rng.Next();
+
+            Console.WriteLine(randomNumber);
+        }
+    }
+}
